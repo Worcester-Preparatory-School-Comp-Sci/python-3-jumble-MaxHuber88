2883bed51bd5bbe3dda12cc24455a6d2d71f85c5
MaxHuber88 <55286559+MaxHuber88@users.noreply.github.com>
2019-09-30 11:09:49 -0400

    Update Scrambler Game.py

--- Scrambler Game.py
+++ Scrambler Game.py
@@ -1,6 +1,6 @@
 import random
-
-print("Hi! Welcome to the Scrambler!\nEnter 0 to exit.")
+# MAX HUBER 9/30
+print("Hi! Welcome to the Scrambler!\nEnter 0 to exit.") # welcome player, init variables
 wordlist = ["dog","cat","fish","banana"]
 scrambledword = ["","","","","","","","","","","","","","",""]
 scramble = ""
@@ -8,14 +8,15 @@ playing = True
 foundPlace= False
 trying = True
 
-while playing == True:
+while playing == True: #this while loop only ends when the player enters 0 to exit
+	# reset variables each time new word is to be guessed
 	scrambledword = ["","","","","","","","","","","","","","",""]
 	scramble = ""
 	foundPlace= False
 	trying = True
 	
-	selected = wordlist[random.randint(0,len(wordlist) - 1)]
-	for x in range(0,len(selected)):
+	selected = wordlist[random.randint(0,len(wordlist) - 1)] #select a random word from the list
+	for x in range(0,len(selected)): #scramble word
 		foundPlace = False
 		while foundPlace == False:
 			i = random.randint(0,len(selected) - 1)
@@ -23,9 +24,9 @@ while playing == True:
 				scrambledword[i] = selected[x]
 				foundPlace = True
 	scramble = scrambledword[0]+scrambledword[1]+scrambledword[2]+scrambledword[3]+scrambledword[4]+scrambledword[5]+scrambledword[6]+scrambledword[7]+scrambledword[8]+scrambledword[9]+scrambledword[10]+scrambledword[11]+scrambledword[12]+scrambledword[13]+scrambledword[14]
-	print(scramble)
+	print(scramble) #print scrambled word
 	
-	while trying == True:
+	while trying == True: #this loop only ends when player guesses correctly or enters 0 to exit
 		guess = input("^  What is this word?" +"\n")
 		if guess == selected:
 			print("Yes! Try another one!")
